f02f186d36b2c818627380575cffcddc456b9e95
= <=>
2024-07-22 12:15:50 +0300

    created prepareGoods function

--- React/list-sorting/src/App.jsx
+++ React/list-sorting/src/App.jsx
@@ -5,28 +5,40 @@ import "./App.scss";
 import products from "./products.json";
 import { GoodList } from "./components/GoodList/GoodList";
 import { Button } from "./components/Button/Button";
-// import { TRUE } from "sass";
 
-const ID_BUTTON_NAME = "Id";
-const NAME_BUTTON_NAME = "Name";
-const COLOR_BUTTON_NAME = "Color";
-const RESET_BUTTON_NAME = "Reset";
+const ID_BUTTON_NAME = "id";
+const NAME_BUTTON_NAME = "name";
+const COLOR_BUTTON_NAME = "color";
+const RESET_BUTTON_NAME = "reset";
 
+function prepareGoods(goods, { sortButton, query }) {
+  let preparedGoods = [...goods];
+
+  if (query) {
+    preparedGoods = preparedGoods.filter(good => good.name.includes(query));
+  }
+
+  if (sortButton) {
+    preparedGoods.sort((product1, product2) => {
+      switch (sortButton) {
+        case ID_BUTTON_NAME:
+          return product1[sortButton] - product2[sortButton];
+        
+        case NAME_BUTTON_NAME:
+        case COLOR_BUTTON_NAME:
+          return product1[sortButton].localeCompare(product2[sortButton]);
+        default:
+          return 0;
+      }
+    });
+  }
+  
+  return preparedGoods;
+}
 const App = () => {
   const [sortButton, setSortButton] = useState("");
+  const visibleProducts = prepareGoods(products, {sortButton});
 
-  const visibleProducts = products.toSorted((product1, product2) => {
-    switch (sortButton) {
-      case ID_BUTTON_NAME:
-        return product1.id - product2.id;
-      case NAME_BUTTON_NAME:
-        return product1.name.localeCompare(product2.name);
-      case COLOR_BUTTON_NAME:
-        return product1.color.localeCompare(product2.color);
-      default:
-        return 0;
-    }
-  });
 
   return (
     <div className="App">
